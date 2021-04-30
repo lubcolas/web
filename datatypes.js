0e42363397f6d6008602f4cae3e675bc23877bd0
lubcolase <lubenscolas@MacBook-Air-de-Lubens.local>
2021-04-30 19:19:32 -0300

    30 Avril 2021

---
+++ datatypes.js
@@ -0,0 +1,16 @@
+// The undefined type
+
+var x;
+var y = [10, 20];
+var z = {alpha: 33};
+//var v;
+ 
+console.log(x); // undefined
+ 
+console.log(y[1]); // 20
+console.log(y[3]); // undefined
+ 
+console.log(z.alpha); // 33
+console.log(z.beta); // undefined
+ 
+console.log(v); // ReferenceError
---
+++ object.js
@@ -0,0 +1,20 @@
+var carName = "Volvo";
+
+// code here can use carName
+
+function myFunction() {
+  // code here can also use carName
+}
+
+
+// JS Array
+var cars = [
+    "Saab",
+    "Volvo",
+    "BMW"
+  ];
+
+
+// JS The join() method
+  var fruits = ["Banana", "Orange", "Apple", "Mango"];
+document.getElementById("demo").innerHTML = fruits.join(" * ");
